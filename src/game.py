04208f89902119c18c737f2076e986bed82947af
Diego <diego@di>
2026-06-15 16:23:00 -0300

    Creating assets folder Inserting all assets Creating the menu: background, sound and fonts Creating the const file

--- src/game.py
+++ src/game.py
@@ -1,14 +1,17 @@
 import pygame
+
+from src.const import WIN_HEIGHT, WIN_WIDTH
 from src.menu import Menu
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode((600,400))
+        self.window = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
         pygame.display.set_caption("Montain Shooter")
 
     def run(self):
         menu = Menu(self.window)
+
         while True:
             menu.run()
             # Check for all events
