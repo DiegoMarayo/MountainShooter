287b815649da038613f060a3208141369c3b1dcb
Diego <diego@di>
2026-06-20 17:25:23 -0300

    Level 2 implementation

--- src/entityFactory.py
+++ src/entityFactory.py
@@ -4,22 +4,29 @@ from src.const import WIN_WIDTH, WIN_HEIGHT
 from src.enemy import Enemy
 from src.player import Player
 
+
 class EntityFactory:
 
     @staticmethod
-    def get_entity(entity_name: str, position=(0,0)):
+    def get_entity(entity_name: str, position=(0, 0)):
         match entity_name:
-            case 'Level1bg':
+            case 'Level1Bg':
                 list_bg = []
-                for i in range(7):
+                for i in range(7):  # Level1Bg images number
                     list_bg.append(Background(f'Level1bg{i}', position))
-                    list_bg.append(Background(f'Level1bg{i}', position=(WIN_WIDTH,0)))
+                    list_bg.append(Background(f'Level1bg{i}', position=(WIN_WIDTH, 0)))
+                return list_bg
+            case 'Level2Bg':
+                list_bg = []
+                for i in range(5):  # Level2Bg images number
+                    list_bg.append(Background(f'Level2bg{i}', position))
+                    list_bg.append(Background(f'Level2bg{i}', position=(WIN_WIDTH, 0)))
                 return list_bg
             case 'Player1':
                 return Player('Player1', (10, WIN_HEIGHT / 2 - 30))
             case 'Player2':
                 return Player('Player2', (10, WIN_HEIGHT / 2 + 30))
             case 'Enemy1':
-                return  Enemy('Enemy1', (WIN_WIDTH + 10, random.randint(40, WIN_HEIGHT - 40)))
+                return Enemy('Enemy1', (WIN_WIDTH + 10, random.randint(40, WIN_HEIGHT - 40)))
             case 'Enemy2':
                 return Enemy('Enemy2', (WIN_WIDTH + 10, random.randint(40, WIN_HEIGHT - 40)))
--- src/game.py
+++ src/game.py
@@ -16,11 +16,13 @@ class Game:
 
             # Option Menu Action
             if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:
-                level = Level(self.window, 'level', menu_return)
-                level_return = level.run()
-                pass
-            elif menu_return == MENU_OPTION[3]:
-                pass
+                player_score = [0, 0] # [ Player1, Player2 ]
+                level = Level(self.window, 'Level1', menu_return, player_score)
+                level_return = level.run(player_score)
+                if level_return:
+                    level = Level(self.window, 'Level2', menu_return)
+                    level_return = level.run(player_score)
+
             elif menu_return == MENU_OPTION[4]:
                 pygame.quit() # Quit Game
                 quit()
